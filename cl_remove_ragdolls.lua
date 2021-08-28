63a48fa4963a587a64ca71dc69cd898a58850364
realpack <packageschannel@gmail.com>
2021-08-29 07:01:36 +1000

    sh_remove_ragdolls.lua > cl_remove_ragdolls.lua

---
+++ cl_remove_ragdolls.lua
@@ -0,0 +1,20 @@
+RunConsoleCommand("r_decals", 10)
+RunConsoleCommand("mp_decals", 10)
+
+hook.Add("NeedsDepthPass", "RemoveRenderDepth", function()
+	return false
+end)
+
+timer.Create("CleanBodys", 60, 0, function()
+	RunConsoleCommand("r_cleardecals")
+
+	for k, v in ipairs(ents.FindByClass("class C_ClientRagdoll")) do
+		v:Remove()
+	end
+
+	for k, v in ipairs(ents.FindByClass("class C_PhysPropClientside")) do
+		if not IsValid(v:GetParent()) then
+			v:Remove()
+		end
+	end
+end)
